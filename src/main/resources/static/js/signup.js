aed63b6f8f484016d54c9132a02c6dcdf2a1fd4e
DogGang <rlaehgusqp@naver.com>
2022-01-25 00:35:16 +0900

    board_list,add,form,list html파일에 이미지파일 오류나는거 경로 수정함. 그리고 signup페이지 아무 값도 안넣고 admin password 입력시 로그인 box보다 로그인 버튼이 밖으로 튀어나오게 하는거 수정함

--- src/main/resources/static/js/signup.js
+++ src/main/resources/static/js/signup.js
@@ -7,12 +7,15 @@ const loginForm = document.getElementById('login_form');
 const idNullChkText = document.getElementById('login_id_null_notice');
 const passNullChkText = document.getElementById('login_pass_null_notice');
 
+const signUpBox = document.getElementById('box1');
+
 function loginNullChk(e){
     if(loginForm.email.value=="" && loginForm.password.value=="" && loginForm.passver.value==""){
         e.preventDefault();
         document.loginForm.email.focus();
         idNullChkText.classList.remove('hidden');
         passNullChkText.classList.remove('hidden');
+        signUpBox.style.height='580px';
     }else if(loginForm.email.value==""){
         e.preventDefault();
         document.loginForm.email.focus();
@@ -53,7 +56,6 @@ function adminPasswordChk(event){
     const signUpBox = document.getElementById('box1');
     if(adminPasswordInput.value=='asdf'){
         adminWrap.classList.remove('hidden');
-        signUpBox.style.height='520px';
         alert('관리자 계정으로 회원가입이 가능합니다.');
     }else{
         alert('관리자만 입력해주세요.');
