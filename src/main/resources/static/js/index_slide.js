b43905aab328af0b4bd2a11b7ff567e72b46cdf8
DogGang <rlaehgusqp@naver.com>
2022-02-03 16:47:26 +0900

    index 만드는중

--- src/main/resources/static/js/index_slide.js
+++ src/main/resources/static/js/index_slide.js
@@ -1,1 +1,10 @@
-$('.single-item').slick();
+$(document).ready(function(){ $('.bxslider').bxSlider({
+    auto: true,  // 자동 슬라이드 
+    controls: true, // prev/next 버튼 노출 여부 
+    pager: true,
+    pause: 5000,
+    autoHover: true,
+    maxSlides: 1,
+    minSlides: 1,
+    });
+});
