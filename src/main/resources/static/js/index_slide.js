3603674c4c6b3eb6d0cbb8ce1641e31ed6983fcc
DogGang <rlaehgusqp@naver.com>
2022-02-07 01:17:05 +0900

    owl slide footer 배너 js 추가

--- src/main/resources/static/js/index_slide.js
+++ src/main/resources/static/js/index_slide.js
@@ -23,15 +23,3 @@ $(document).ready(function(){
 });
   });
 
-//navbar
-$(document).ready(function(){
-  
-	$('#main_menu > li > a').click(function(){
-	  $(this).next($('.snd_menu')).slideToggle('fast');
-	})
-	$('.snd_menu > li > a').click(function(e){
-	  e.stopPropagation();
-	  $(this).next($('.trd_menu')).slideToggle('fast');
-	})
-	
-  })
