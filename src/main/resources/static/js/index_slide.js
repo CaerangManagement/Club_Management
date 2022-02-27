fa4eb818459885a324f6954c79ce25135b4eaf9c
DogGang <rlaehgusqp@naver.com>
2022-02-27 16:02:18 +0900

    max-slider 개수 수정

--- src/main/resources/static/js/index_slide.js
+++ src/main/resources/static/js/index_slide.js
@@ -4,9 +4,9 @@ $(document).ready(function(){ $('.bxslider').bxSlider({
     auto: true,  // 자동 슬라이드 
     controls: true, // prev/next 버튼 노출 여부 
     pager: true,
-    pause: 5000,
+    pause: 10000,
     autoHover: true,
-    maxSlides: 1,
+    maxSlides: 8,
     minSlides: 1,
     });
 });
